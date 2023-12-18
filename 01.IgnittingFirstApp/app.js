fa286bdd535b5a6b582373279847be2bb0e25550
Utkarsh <utkarshbhujbal02@gmail.com>
2023-12-18 22:43:26 +0530

    Third Commit

--- 01.IgnittingFirstApp/app.js
+++ 01.IgnittingFirstApp/app.js
@@ -1,3 +1,40 @@
+
+/*Steps for Parcel  app 
+         1/. npm init 
+         2.npm install -D parcel 
+         3.npm install react   
+         4. npm i react-dom   
+         5.npx parcel index.html
+
+
+
+*/
+
+/*
+
+*Pros of using parcel
+
+* HMR = hot module reload
+* File Watcher algorithm - c++
+* BUNDLING 
+* Minify code
+* Cleaning our code : reduce console vlog etc
+* Super fast build algorithm
+* Image optimization 
+* Dev abd production Build
+* Caching while development
+* compression
+* compatible with older version of browsers
+* Gives us option to build or run app on https- we have to use --https 
+   example: npx parcel index.html --https
+* manage port number
+* parcel is zero config 
+
+
+Transitive dependancies
+
+*/
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 
